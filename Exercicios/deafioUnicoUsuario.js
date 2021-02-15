0bc8c5c9d0f693cd2eb6d148f4e2af4dd85cc620
lucas <lucadauto@gmail.com>
2021-02-14 21:22:38 -0300

    primeiroBot e desafio

---
+++ Exercicios/deafioUnicoUsuario.js
@@ -0,0 +1,18 @@
+const env = require('../.env')
+const Telegraf = require('telegraf')
+
+const bot =  new Telegraf(env.token)
+
+bot.start(ctx => {
+    const from = ctx.update.message.from
+    const usuario = '';
+
+    if(from.id == usuario){
+        ctx.reply(`Ao seu dispor, ${from.first_name} meu mestre!`)
+    }
+    else {
+        ctx.reply('Sinto muito, mas só falo com o meu mestre')
+    }
+})
+
+bot.startPolling()
